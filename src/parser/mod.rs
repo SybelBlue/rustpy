5abb488fe13bbbee665fe0876f13a20344bac417
L Caraco <lbc.dna@gmail.com>
2021-04-30 10:32:06 -0500

    lots of progress

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,14 +1,54 @@
+use std::fmt::Display;
+
 pub mod lexer;
 
 pub type ParseResult<T> = Result<T, ParseError>;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct ParseError {
-    pub msg: String,
-    pub src_path: String,
-    pub line: usize,
-    pub col: usize,
+    msg: String,
+    src_path: Option<String>,
+    line: usize,
+    col: usize,
+}
+
+impl ParseError {
+    fn from_str(msg: String, line: usize, col: usize) -> Self {
+        Self { msg, line, col, src_path: None }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Error parsing {}\n\t{}", 
+            self.src_path.clone().map_or(
+                String::from("string"), 
+                |f_p| format!("file at {}:{}:{}", f_p, self.line, self.col)
+            ),
+            self.msg
+        )
+    }
 }
 
 trait ParseStream<T> {
     fn next(&mut self) -> ParseResult<T>;
     fn try_match(&mut self, items: Vec<T>) -> ParseResult<Vec<T>>;
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct StringStream {
+    line: usize,
+    col: usize,
+    pos: usize,
+    data: String,
+}
+
+impl ParseStream<char> for String {
+    fn next(&mut self) -> ParseResult<char> {
+        todo!()
+    }
+
+    fn try_match(&mut self, items: Vec<char>) -> ParseResult<Vec<char>> {
+        todo!()
+    }
 }
