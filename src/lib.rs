f878c2f92d1595dbca2e4e2bf15ab0e2792ed38b
L Caraco <lbc.dna@gmail.com>
2021-04-30 14:18:50 -0500

    forked off parse_stream

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,19 @@ pub mod parser;
 #[cfg(test)]
 mod tests {
     mod parser {
-        use crate::parser::{ParseStream, StringStream};
-        #[test]
-        fn str_stream() {
-            let mut ss = StringStream::new("Hello World!");
-            assert!(matches!(ss.next(), Ok('H')));
-            assert!(matches!(ss.try_match("Hello".chars().collect()), Err(_)));
-            assert!(matches!(ss.try_match("ello ".chars().collect()), Ok(_)));
-            assert!(matches!(ss.next(), Ok('W')));
-            assert!(matches!(ss.try_match("orld!".chars().collect()), Ok(_)));
-            assert!(matches!(ss.try_match("orld!".chars().collect()), Err(_)));
-            assert!(matches!(ss.next(), Err(_)));
+        mod parse_stream {
+            use crate::parser::parse_stream::{ParseStream, StringStream};
+            #[test]
+            fn str_stream() {
+                let mut ss = StringStream::new("Hello World!");
+                assert!(matches!(ss.next(), Ok('H')));
+                assert!(matches!(ss.try_match("Hello".chars().collect()), Err(_)));
+                assert!(matches!(ss.try_match("ello ".chars().collect()), Ok(_)));
+                assert!(matches!(ss.next(), Ok('W')));
+                assert!(matches!(ss.try_match("orld!".chars().collect()), Ok(_)));
+                assert!(matches!(ss.try_match("orld!".chars().collect()), Err(_)));
+                assert!(matches!(ss.next(), Err(_)));
+            }
         }
     }
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,4 +1,5 @@
 pub mod lexer;
+pub mod parse_stream;
 
 pub type ParseResult<T> = Result<T, ParseError>;
 
@@ -55,66 +56,5 @@ impl std::fmt::Display for ParseError {
             ),
             self.msg
         )
-    }
-}
-
-pub trait ParseStream<T> {
-    fn next(&mut self) -> ParseResult<T>;
-    fn try_match(&mut self, items: Vec<T>) -> ParseResult<Vec<T>>;
-}
-
-impl<T> Iterator for dyn ParseStream<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next().ok()
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct StringStream {
-    file_pos: FilePos,
-    pos: usize,
-    data: Vec<char>,
-}
-
-impl StringStream {
-    pub fn new(text: &str) -> Self {
-        Self { file_pos: FilePos::new(), pos: 0, data: text.chars().collect() }
-    }
-
-    fn check_eof(&self) -> ParseResult<()> {
-        if self.pos >= self.data.len() {
-            Err(ParseError::eof(self.file_pos, None))
-        } else {
-            Ok(())
-        }
-    }
-}
-
-impl ParseStream<char> for StringStream {
-    fn next(&mut self) -> ParseResult<char> {
-        self.check_eof()?;
-        let c = self.data[self.pos];
-        self.pos += 1;
-        self.file_pos.advance(&c);
-        Ok(c)
-    }
-
-    fn try_match(&mut self, items: Vec<char>) -> ParseResult<Vec<char>> {
-        self.check_eof()?;
-        let mut f_pos = self.file_pos.clone();
-        for (&a, &b) in self.data[self.pos..].iter().zip(items.iter()) {
-            if a != b {
-                return Err(
-                    ParseError::from_str(format!("Expected {}, got {}", b, a), f_pos)
-                )
-            } else {
-                f_pos.advance(&a)
-            }
-        }
-        self.file_pos = f_pos;
-        self.pos += items.len();
-        Ok(items)
     }
 }
