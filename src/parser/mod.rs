dd1318aa2fdcaba7b30ebc665cd39520ee08055c
L Caraco <lbc.dna@gmail.com>
2021-04-30 10:15:13 -0500

    made parseresult

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,9 @@
 pub mod lexer;
+
+pub type ParseResult<T> = Result<T, ParseError>;
+pub struct ParseError {
+    pub msg: String,
+    pub src_path: String,
+    pub line: usize,
+    pub col: usize,
+}
