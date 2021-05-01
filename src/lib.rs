b06de87f049e68989d8f1c0c6d8034d62f29037c
L Caraco <lbc.dna@gmail.com>
2021-05-01 16:00:45 -0500

    worked out whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,32 @@ pub mod parser;
 mod tests {
     mod parser {
         mod parse_stream {
-            use crate::parser::parse_stream::{ParseStream, StringStream};
+            use crate::parser::{lexer::TokenStream, parse_stream::{ParseStream, StringStream}};
             #[test]
             fn str_stream() {
                 let mut ss = StringStream::new("Hello World!");
-                assert!(matches!(ss.next(), Ok('H')));
+                assert!(matches!(ss.parse_next(), Ok('H')));
                 assert!(matches!(ss.try_match("Hello".chars().collect()), Err(_)));
                 assert!(matches!(ss.try_match("ello ".chars().collect()), Ok(_)));
-                assert!(matches!(ss.next(), Ok('W')));
+                assert!(matches!(ss.parse_next(), Ok('W')));
                 assert!(matches!(ss.try_match("orld!".chars().collect()), Ok(_)));
                 assert!(matches!(ss.try_match("orld!".chars().collect()), Err(_)));
-                assert!(matches!(ss.next(), Err(_)));
+                assert!(matches!(ss.parse_next(), Err(_)));
+            }
+
+            #[test]
+            fn lexing() {
+                use crate::parser::lexer::{Token::{self, *}, Keyword::*};
+                let ss = StringStream::new("def func(a, b):\n\tx = a + \tb\n\treturn    x\n\n");
+                let tokens = TokenStream::new(ss);
+                let res = 
+                vec![ WS(0), Kywrd(Def), Ident(String::from("func")), Symbl('('), Ident(String::from("a")), Symbl(','), Ident(String::from("b")), Symbl(')'), Symbl(':')
+                    , WS(4), Ident(String::from("x")), Symbl('='), Ident(String::from("a")), Symbl('+'), Ident(String::from("b"))
+                    , WS(4), Kywrd(Return), Ident(String::from("x"))
+                    , WS(0)
+                    , WS(0)
+                ];
+                assert_eq!(tokens.collect::<Vec<Token>>(), res);
             }
         }
     }
--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, fmt::Display};
+use std::{collections::VecDeque, fmt::Display, iter::Peekable};
 
 use crate::parser::*;
 
@@ -32,6 +32,8 @@ pub enum Keyword {
     None,
     True,
     False,
+    Return,
+    Class,
 }
 
 fn into_token(s: String) -> Token {
@@ -40,6 +42,8 @@ fn into_token(s: String) -> Token {
         "if" => Token::Kywrd(If),
         "else" => Token::Kywrd(Else),
         "def" => Token::Kywrd(Def),
+        "return" => Token::Kywrd(Return),
+        "class" => Token::Kywrd(Class),
         "None" => Token::Kywrd(None),
         "True" => Token::Kywrd(True),
         "False" => Token::Kywrd(False),
@@ -48,16 +52,16 @@ fn into_token(s: String) -> Token {
 }
 
 fn as_symbol(c: char) -> Option<Token> {
-    if matches!(c, '.' | ',' | ':' | '(' | ')' |'[' | ']' | '+' | '-' | '#') {
+    if matches!(c, '.' | ',' | ':' | '(' | ')' |'[' | ']' | '=' | '+' | '-' | '#' | '\'' | '\\') {
         Some(Token::Symbl(c))
     } else {
         None
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub struct TokenStream<C : Iterator<Item = char>> {
-    char_iter: C,
+    char_iter: Peekable<C>,
     cached: VecDeque<Token>,
     file_pos: FilePos,
     line_start: bool,
@@ -65,7 +69,7 @@ pub struct TokenStream<C : Iterator<Item = char>> {
 
 impl<C : Iterator<Item = char>> TokenStream<C> {
     pub fn new(char_iter: C) -> Self {
-        Self { char_iter, file_pos: FilePos::new(), cached: VecDeque::new(), line_start: true }
+        Self { char_iter: char_iter.peekable(), file_pos: FilePos::new(), cached: VecDeque::new(), line_start: true }
     }
 }
 
@@ -74,24 +78,48 @@ impl<C : Iterator<Item = char>> ParseStream<Token> for TokenStream<C> {
         if let Some(item) = self.cached.pop_front() {
             return Ok(item);
         }
+        if self.line_start {
+            self.line_start = false;
+            let mut n = 0;
+            while let Some(&c) = self.char_iter.peek() {
+                self.file_pos.advance(c);
+                match c {
+                    ' ' => n += 1,
+                    '\t' => n += 4,
+                    _ => return Ok(Token::WS(n)),
+                }
+                self.char_iter.next();
+            }
+            return Ok(Token::WS(n));
+        }
         let mut built = String::new();
         for c in &mut self.char_iter {
+            let file_pos = self.file_pos;
+            self.file_pos.advance(c);
+            
             if c.is_whitespace() { 
+                if c == '\n' {
+                    self.line_start = true;
+                }
                 if !built.is_empty() {
-                    return Ok(into_token(built));
+                    return Ok(into_token(built))
+                }
+                if self.line_start {
+                    return self.parse_next();
                 }
             } else if let Some(tkn) = as_symbol(c) {
-                return Ok(if !built.is_empty() {
-                    self.cached.push_back(tkn);
-                    into_token(built)
-                } else {
-                    tkn
-                })
+                return Ok(
+                    if !built.is_empty() {
+                        self.cached.push_back(tkn);
+                        into_token(built)
+                    } else {
+                        tkn
+                    }
+                )
             } else if matches!(c, '0'..='9' | 'a'..='z' | 'A'..='Z' | '_') {
                 built.push(c);
-                self.file_pos.advance(c);
             } else {
-                return Err(ParseError::from_str(format!("Bad char '{}'", c), self.file_pos));
+                return Err(ParseError::from_str(format!("Bad char '{}'", c), file_pos));
             }
         }
         
